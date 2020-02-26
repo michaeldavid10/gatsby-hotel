bcca256affd42bc6146316b6d0fb7c03f32cca24
Michael David <mdavidmachacuay@gmail.com>
2020-02-26 12:25:10 -0500

    header css

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,32 @@
 import React from "react"
+import {css} from "@emotion/core"
 
 const Header = () => (
-  <h1>Header</h1>
+  <header
+    css={css`
+      background-color: #333;
+      padding: 1rem;
+    `}
+  >
+    <div
+      css={css`
+        max-width: 1200px;
+        margin: 0 auto;
+        @media (min-width: 768px) {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+      `}
+    >
+      <h1
+        css={css`
+          color: #FFF;
+          text-align: center;
+        `}
+      >Header</h1>
+    </div>
+  </header>
 )
 
 export default Header
