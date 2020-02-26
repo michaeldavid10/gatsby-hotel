5700db3f7b67550ee5b5b3bb6f884c215773db68
Michael David <mdavidmachacuay@gmail.com>
2020-02-26 12:12:52 -0500

    adding global css

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,6 +7,8 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import {Global, css} from "@emotion/core"
 
 import Header from "./header"
 import "./layout.css"
@@ -14,6 +16,38 @@ import "./layout.css"
 const Layout = ({ children }) => {
   return (
     <>
+      <Global
+        styles={css`
+          html {
+            font-size: 62.5%;
+          }
+          body {
+            font-size: 16px;
+            font-size: 1.6rem;
+            line-height: 1.5;
+          }
+          h1, h2, h3 {
+            margin: 0;
+            line-height: 1.5;
+          }
+          h1, h2 {
+            font-family: 'Roboto', serif;
+          }
+          h3 {
+            font-family: 'PT Sans', sans-serif;
+          }
+          ul {
+            list-style: none;
+            margin: 0;
+            pagging: 0;
+          }
+        `}
+      />
+      <Helmet>
+        <title>Gatsby Hotel</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
+        <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto:400,700&display=swap" rel="stylesheet" />
+      </Helmet>
       <Header />
       {children}
     </>
