9d081e9f569ee3a0491e0da67d73e0700231ba9b
Michael David Machacuay <mdavidmachacuay@gmail.com>
2020-10-27 11:48:25 -0500

    about us css

--- src/components/aboutUsContent.js
+++ src/components/aboutUsContent.js
@@ -1,6 +1,25 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import Image from 'gatsby-image'
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
+
+const Content = styled.main`
+  padding-top: 4rem;
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 3rem;
+  }
+  p {
+    line-height: 2;
+    margin-top: 2rem;
+  }
+`
 
 const AboutUsContent = () => {
   const result = useStaticQuery(graphql`
@@ -23,11 +42,19 @@ const AboutUsContent = () => {
 
   return (
     <>
-      <h2>{titulo}</h2>
-      <div>
+      <h2
+        css={css`
+          margin-top: 4rem;
+          text-align: center;
+          font-size: 4rem;
+        `}
+      >
+        {titulo}
+      </h2>
+      <Content>
         <p>{contenido}</p>
         <Image fluid={imagen.fluid} />
-      </div>
+      </Content>
     </>
   )
 }
